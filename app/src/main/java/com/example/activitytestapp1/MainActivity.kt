9fbe3387f34648042bf6d247d7256c1ff945d9b3
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-06-01 21:02:21 +0545

    Version 1 that shows a toast stating restarted when onRestart activity occurs

---
+++ app/src/main/java/com/example/activitytestapp1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.activitytestapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"restarted",Toast.LENGTH_LONG).show()
+    }
+}
